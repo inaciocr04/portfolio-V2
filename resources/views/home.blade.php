9550aff4aede8a13f32538c90793957f0f41159d
inaciocr04 <inacio.rodrigues@etu.unistra.fr>
2024-12-04 22:55:43 +0100

    initalisation couleur et layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,3 @@
+<x-layout.main>
+    <h1 class="bg-red-700">coucou</h1>
+</x-layout.main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,30 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                poppins: ['Poppins', 'sans-serif'],
+            },
+            colors: {
+                'blue-bg-primary-color': '#075275',
+                'blue-primary-color': '#13293D',
+                'blue-secondary-color': '#006494',
+                'blue-third-color': '#1D94CC',
+                'blue-fourth-color': '#2C465E',
+                'blue-fifth-color': '#1BB3FA',
+                'blue-sixth-color': '#E8F1F2',
+                'blue-seventh-color': '#22B9C9',
+                'beige-background-color': '#efe7e5',
+                'beige-primary-color': '#F5F7FA',
+                'beige-secondary-color': '#3E4C59',
+                'beige-third-color': '#ecd9d5',
+                'beige-fourth-color': '#f8ab9b',
+                'beige-fifth-color': '#3E4C59',
+                'beige-sixth-color': '#7B8794',
             },
         },
     },
-    plugins: [],
+    plugins: [
+        require('@tailwindcss/forms')({
+            strategy: 'class'
+        })
+    ],
 };
