d1a38f17514dea2a388ba44ad3edb8ee96d54a88
inaciocr04 <inaciocr04@gmail.com>
2024-12-05 13:59:04 +0100

    css en cours de construction

--- resources/views/components/layout/main.blade.php
+++ resources/views/components/layout/main.blade.php
@@ -5,11 +5,40 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     @vite(['resources/js/app.js', 'resources/css/app.css'])
 
     <title>Portfolio</title>
 </head>
-<body>
-    {{$slot}}
+<body class="bg-beige-primary-color">
+    <nav class="flex justify-between items-center border-2 border-black w-screen">
+        <img src="{{ asset('img/logo_dark.png') }}" alt="Logo" class="w-32">
+        <ul class="flex space-x-16 bg-blue-primary-color text-white rounded-full px-8 py-4">
+            <li>Accueil</li>
+
+            <li>Mes projets</li>
+
+            <li>Mes Passions</li>
+
+            <li>Contact</li>
+        </ul>
+        <div class="flex space-x-6 items-center">
+            <div class="border w-10 flex justify-center items-center p-2 h-10 border-black rounded-full size-6">
+                <i class="bi bi-github text-2xl"></i>
+            </div>
+            <div class="border w-10 flex justify-center items-center p-2 h-10 border-black rounded-full size-6">
+                <i class="bi bi-instagram text-2xl"></i>
+            </div>
+            <div class="border w-10 flex justify-center items-center p-2 h-10 border-black rounded-full size-6">
+                <i class="bi bi-twitter-x text-2xl"></i>
+            </div>
+        </div>
+    </nav>
+    <main class="px-20">
+        {{$slot}}
+    </main>
+<footer class="bg-beige-fourth-color p-8 text-center">
+    <p>&copy;2024 Inacio Rodrigues</p>
+</footer>
 </body>
 </html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,29 @@
 <x-layout.main>
-    <h1 class="bg-red-700">coucou</h1>
+    <div class="flex p-16 bg-blue-fifth-color rounded-2xl m-10">
+        <div>
+            <img src="{{ asset('img/photo-moi.png') }}" alt="ordinateur" class="">
+        </div>
+        <div>
+            <small>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                </svg>
+                Bas Rhin, France
+            </small>
+            <h1 data-aos="fade-right" data-aos-duration="1000">Inácio Rodrigues</h1>
+            <h2 data-aos="fade-right" data-aos-duration="3000">
+                <span class="multitext color_span"></span>
+            </h2>
+            <p data-aos="fade-right" data-aos-duration="3000">
+                Actuellement titulaire d'un baccalauréat STI2D (Sciences et
+                Technologies de l'Industrie et du Développement Durable), je poursuis
+                mes études à l'IUT de Haguenau dans la filière MMI (Métiers du
+                Multimédia et de l'Internet) en vue d'obtenir mon diplôme de niveau
+                BAC +3. Je suis actuellement à la recherche d'un stage de 10 semaines,
+                ainsi que d'une alternance d'une durée d'un an ou d'un an et demi.
+            </p>
+        </div>
+
+    </div>
 </x-layout.main>
